d98fb2f08fa747907f7ac008d802aa8a6560093c
morganpare <morganpare@gmail.com>
2019-07-18 12:09:22 +0100

    commenting out creation of environment

--- env_setup.sh
+++ env_setup.sh
@@ -3,5 +3,5 @@ export VIRTUALENVWRAPPER_PYTHON=/bin/python3
 export VIRTUALENVWRAPPER_PYTHON=/bin/python3.6
 export VIRTUALENVWRAPPER_VIRTUALENV=/bin/virtualenv
 source /bin/virtualenvwrapper.sh
-mkvirtualenv NBA # only if required
+# mkvirtualenv NBA # only if required
 workon NBA
